41da47201fae24787598c2613025f8afeb6a5af0
Nay36 <santosnayaracosta@gmail.com>
2025-10-19 16:23:22 -0300

    Update lista.cpp

--- lista.cpp
+++ lista.cpp
@@ -17,7 +17,7 @@ Dici* carrega_inic(Dici* ptr){
     ptr = insere_ord(ptr, "Dentro", "In");
     ptr = insere_ord(ptr, "Eh", "Is");
     ptr = insere_ord(ptr, "Voce", "You");
-    ptr = insere_ord(ptr, "Aquele", "That");
+    ptr = insere_ord(ptr, "Aquele", "Such");
     ptr = insere_ord(ptr, "Menino", "Boy");
     ptr = insere_ord(ptr, "Menina", "Girl");
     ptr = insere_ord(ptr, "Falso", "Fake");
@@ -30,9 +30,76 @@ Dici* carrega_inic(Dici* ptr){
     ptr = insere_ord(ptr, "Bonito", "Pretty");
     ptr = insere_ord(ptr, "De", "Of");
     ptr = insere_ord(ptr, "E", "And");
-    ptr = insere_ord(ptr, "Um", "A");
+    ptr = insere_ord(ptr, "Um", "One");
     ptr = insere_ord(ptr, "Uma", "A");
     ptr = insere_ord(ptr, "Este", "It");
+    ptr = insere_ord(ptr, "Amor", "Love");
+    ptr = insere_ord(ptr, "Tempo", "Time");
+    ptr = insere_ord(ptr, "Vida", "Life");
+    ptr = insere_ord(ptr, "Mundo", "World");
+    ptr = insere_ord(ptr, "Casa", "House");
+    ptr = insere_ord(ptr, "Amigo", "Friend");
+    ptr = insere_ord(ptr, "Trabalho", "Work");
+    ptr = insere_ord(ptr, "Comer", "Eat");
+    ptr = insere_ord(ptr, "Beber", "Drink");
+    ptr = insere_ord(ptr, "Feliz", "Happy");
+    ptr = insere_ord(ptr, "Triste", "Sad");
+    ptr = insere_ord(ptr, "Carro", "Car");
+    ptr = insere_ord(ptr, "Livro", "Book");
+    ptr = insere_ord(ptr, "Escola", "School");
+    ptr = insere_ord(ptr, "Professor", "Teacher");
+    ptr = insere_ord(ptr, "Aluno", "Student");
+    ptr = insere_ord(ptr, "Dinheiro", "Money");
+    ptr = insere_ord(ptr, "Familia", "Family");
+    ptr = insere_ord(ptr, "Pai", "Father");
+    ptr = insere_ord(ptr, "Mae", "Mother");
+    ptr = insere_ord(ptr, "Irmao", "Brother");
+    ptr = insere_ord(ptr, "Irma", "Sister");
+    ptr = insere_ord(ptr, "Cidade", "City");
+    ptr = insere_ord(ptr, "Pais", "Country");
+    ptr = insere_ord(ptr, "Hoje", "Today");
+    ptr = insere_ord(ptr, "Amanha", "Tomorrow");
+    ptr = insere_ord(ptr, "Ontem", "Yesterday");
+    ptr = insere_ord(ptr, "Noite", "Night");
+    ptr = insere_ord(ptr, "Manha", "Morning");
+    ptr = insere_ord(ptr, "Tarde", "Afternoon");
+    ptr = insere_ord(ptr, "Rapido", "Fast");
+    ptr = insere_ord(ptr, "Devagar", "Slow");
+    ptr = insere_ord(ptr, "Grande", "Big");
+    ptr = insere_ord(ptr, "Pequeno", "Small");
+    ptr = insere_ord(ptr, "Velho", "Old");
+    ptr = insere_ord(ptr, "Novo", "New");
+    ptr = insere_ord(ptr, "Porfavor", "Please");
+    ptr = insere_ord(ptr, "Desculpa", "Sorry");
+    ptr = insere_ord(ptr, "Ajuda", "Help");
+    ptr = insere_ord(ptr, "Cachorro", "Dog");
+    ptr = insere_ord(ptr, "Gato", "Cat");
+    ptr = insere_ord(ptr, "Comida", "Food");
+    ptr = insere_ord(ptr, "Musica", "Music");
+    ptr = insere_ord(ptr, "Computador", "Computer");
+    ptr = insere_ord(ptr, "Porta", "Door");
+    ptr = insere_ord(ptr, "Janela", "Window");
+    ptr = insere_ord(ptr, "Sol", "Sun");
+    ptr = insere_ord(ptr, "Chuva", "Rain");
+    ptr = insere_ord(ptr, "Ela", "She");
+    ptr = insere_ord(ptr, "Dela", "Her");
+    ptr = insere_ord(ptr, "Ele", "He");
+    ptr = insere_ord(ptr, "Dele", "His");
+    ptr = insere_ord(ptr, "Nos", "Us");
+    ptr = insere_ord(ptr, "Qualquer", "Any");
+    ptr = insere_ord(ptr, "Porque", "Because");
+    ptr = insere_ord(ptr, "Tambem", "Too");
+    ptr = insere_ord(ptr, "Alguns", "Some");
+    ptr = insere_ord(ptr, "Poderia", "Could");
+    ptr = insere_ord(ptr, "Nascer", "Rise");
+    ptr = insere_ord(ptr, "Ninguem", "Nobody");
+    ptr = insere_ord(ptr, "Saber", "Know");
+    ptr = insere_ord(ptr, "Olhar", "Look");
+    ptr = insere_ord(ptr, "Apenas", "Only");
+    ptr = insere_ord(ptr, "Outro", "Other");
+    ptr = insere_ord(ptr, "Primeiro", "First");
+    ptr = insere_ord(ptr, "Dar", "Give");
+    ptr = insere_ord(ptr, "Conselho", "Advice");
 
     ptr = insere_ord(ptr, "oi", "hi");
     ptr = insere_ord(ptr, "bom", "good");
@@ -45,7 +112,7 @@ Dici* carrega_inic(Dici* ptr){
     ptr = insere_ord(ptr, "dentro", "in");
     ptr = insere_ord(ptr, "eh", "is");
     ptr = insere_ord(ptr, "voce", "you");
-    ptr = insere_ord(ptr, "aquele", "that");
+    ptr = insere_ord(ptr, "aquele", "such");
     ptr = insere_ord(ptr, "menino", "boy");
     ptr = insere_ord(ptr, "menina", "girl");
     ptr = insere_ord(ptr, "falso", "fake");
@@ -58,9 +125,76 @@ Dici* carrega_inic(Dici* ptr){
     ptr = insere_ord(ptr, "bonito", "pretty");
     ptr = insere_ord(ptr, "de", "of");
     ptr = insere_ord(ptr, "e", "and");
-    ptr = insere_ord(ptr, "um", "a");
+    ptr = insere_ord(ptr, "um", "one");
     ptr = insere_ord(ptr, "uma", "a");
     ptr = insere_ord(ptr, "este", "it");
+    ptr = insere_ord(ptr, "amor", "love");
+    ptr = insere_ord(ptr, "tempo", "time");
+    ptr = insere_ord(ptr, "vida", "life");
+    ptr = insere_ord(ptr, "mundo", "world");
+    ptr = insere_ord(ptr, "casa", "house");
+    ptr = insere_ord(ptr, "amigo", "friend");
+    ptr = insere_ord(ptr, "trabalho", "work");
+    ptr = insere_ord(ptr, "comer", "eat");
+    ptr = insere_ord(ptr, "beber", "drink");
+    ptr = insere_ord(ptr, "feliz", "happy");
+    ptr = insere_ord(ptr, "triste", "sad");
+    ptr = insere_ord(ptr, "carro", "car");
+    ptr = insere_ord(ptr, "livro", "book");
+    ptr = insere_ord(ptr, "escola", "school");
+    ptr = insere_ord(ptr, "professor", "teacher");
+    ptr = insere_ord(ptr, "aluno", "student");
+    ptr = insere_ord(ptr, "dinheiro", "money");
+    ptr = insere_ord(ptr, "familia", "family");
+    ptr = insere_ord(ptr, "pai", "father");
+    ptr = insere_ord(ptr, "mae", "mother");
+    ptr = insere_ord(ptr, "irmao", "brother");
+    ptr = insere_ord(ptr, "irma", "sister");
+    ptr = insere_ord(ptr, "cidade", "city");
+    ptr = insere_ord(ptr, "pais", "country");
+    ptr = insere_ord(ptr, "hoje", "today");
+    ptr = insere_ord(ptr, "amanha", "tomorrow");
+    ptr = insere_ord(ptr, "ontem", "yesterday");
+    ptr = insere_ord(ptr, "noite", "night");
+    ptr = insere_ord(ptr, "manha", "morning");
+    ptr = insere_ord(ptr, "tarde", "afternoon");
+    ptr = insere_ord(ptr, "rapido", "fast");
+    ptr = insere_ord(ptr, "devagar", "slow");
+    ptr = insere_ord(ptr, "grande", "big");
+    ptr = insere_ord(ptr, "pequeno", "small");
+    ptr = insere_ord(ptr, "velho", "old");
+    ptr = insere_ord(ptr, "novo", "new");
+    ptr = insere_ord(ptr, "porfavor", "please");
+    ptr = insere_ord(ptr, "desculpa", "sorry");
+    ptr = insere_ord(ptr, "ajuda", "help");
+    ptr = insere_ord(ptr, "cachorro", "dog");
+    ptr = insere_ord(ptr, "gato", "cat");
+    ptr = insere_ord(ptr, "comida", "food");
+    ptr = insere_ord(ptr, "musica", "music");
+    ptr = insere_ord(ptr, "computador", "computer");
+    ptr = insere_ord(ptr, "porta", "door");
+    ptr = insere_ord(ptr, "janela", "window");
+    ptr = insere_ord(ptr, "sol", "sun");
+    ptr = insere_ord(ptr, "chuva", "rain");
+    ptr = insere_ord(ptr, "ela", "she");
+    ptr = insere_ord(ptr, "dela", "her");
+    ptr = insere_ord(ptr, "ele", "he");
+    ptr = insere_ord(ptr, "dele", "his");
+    ptr = insere_ord(ptr, "nos", "us");
+    ptr = insere_ord(ptr, "qualquer", "any");
+    ptr = insere_ord(ptr, "porque", "because");
+    ptr = insere_ord(ptr, "tambem", "too");
+    ptr = insere_ord(ptr, "alguns", "some");
+    ptr = insere_ord(ptr, "poderia", "could");
+    ptr = insere_ord(ptr, "nascer", "rise");
+    ptr = insere_ord(ptr, "ninguem", "nobody");
+    ptr = insere_ord(ptr, "saber", "know");
+    ptr = insere_ord(ptr, "olhar", "look");
+    ptr = insere_ord(ptr, "apenas", "only");
+    ptr = insere_ord(ptr, "outro", "other");
+    ptr = insere_ord(ptr, "primeiro", "first");
+    ptr = insere_ord(ptr, "dar", "give");
+    ptr = insere_ord(ptr , "conselho", "advice");
     return ptr;
 }
 
@@ -69,26 +203,6 @@ Dici* alocaNo(){
 }
 void desalocaNo(Dici* ptr){
     delete ptr;
-}
-
-Dici* insere_fim(Dici* ptr, string port, string ing){
-    Dici* novo = alocaNo();
-
-    if(novo != NULL){
-        novo->port = port;
-        novo->ing = ing;
-        novo->prox = NULL;
-    }
-    if(ptr == NULL){
-        return novo;
-    }else{
-        Dici* aux = ptr;
-        while(aux->prox != NULL){
-            aux = aux->prox;
-        }
-        aux->prox = novo;
-    }
-    return ptr;
 }
 
 Dici* insere_ord(Dici* ptr, string port, string ing){
@@ -221,4 +335,6 @@ void imprimir_Dici(Dici* ptr){
     }
 
 }
+
+
 
